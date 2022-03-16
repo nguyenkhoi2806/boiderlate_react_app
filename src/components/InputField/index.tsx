ffb5ce7810ab81e248ab350eec7a4c9b734f71e3
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2022-03-16 11:41:27 +0700

    update input component

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -1,59 +1,23 @@
-import { FormGroup, Input, Label } from 'reactstrap';
-import { InputType } from 'reactstrap/types/lib/Input';
-
-import { SELECT } from '/constants/FieldType';
-
+import InputFieldType from 'components/InputField/InputFieldType';
+import { FormGroup, Label } from 'reactstrap';
 interface InputFieldProps {
   onChange: (value: string | number | [] | boolean) => void;
-  type: InputType;
   label?: string;
   classNameWrapper?: string;
-  placeholder?: string;
-  name?: string;
-  options: { value: string | number; label: string }[];
 }
 
 const defaultProps = {
   label: '',
   classNameWrapper: '',
-  placeholder: '',
-  name: '',
 };
 
 const InputField: React.FC<InputFieldProps> = (props: InputFieldProps) => {
-  const {
-    label,
-    onChange,
-    classNameWrapper,
-    placeholder,
-    type,
-    name,
-    options,
-  } = props;
-
-  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    return onChange(event.target.value);
-  };
+  const { label, classNameWrapper } = props;
 
   return (
     <FormGroup className={classNameWrapper}>
       {label && <Label>{label}</Label>}
-      {type === SELECT ? (
-        <Input name={name} type={SELECT} onChange={onChangeInput}>
-          {options.map((option, key) => (
-            <option key={key} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </Input>
-      ) : (
-        <Input
-          name={name}
-          placeholder={placeholder}
-          type={type}
-          onChange={onChangeInput}
-        />
-      )}
+      <InputFieldType {...props} />
     </FormGroup>
   );
 };
--- src/constants/FieldType.tsx
+++ src/constants/FieldType.tsx
@@ -1,3 +1,5 @@
 export const SELECT = 'select';
 export const CHECKBOX = 'checkbox';
 export const CHECKBOX_GROUP = 'checkbox_group';
+export const RADIO = 'radio';
+export const RADIO_GROUP = 'radio_group';
